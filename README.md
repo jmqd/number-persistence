fc72175d23f816fa1e4a31bd8530dd20e245bb2a
Jordan McQueen <jordan@whoami.sh>
2019-03-30 04:28:31 -0700

    Add semi-optimized search functionality, CLI cmds

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 # Number persistence
 
+## Description
+
 A rust library to calculate the multiplicative [persistence of a number][].
 
 See [OEIS Sequence A003001][].
+
+## Testing
+
+Ran this for the first 10 numbers in the sequence, which returned in a few
+seconds of runtime. Seems to be correct given that it perfectly matches the
+OEIS sequence. I'm sure we could optimize this much further.
+
+```
+jordan@linux-desktop:~/src/number-persistence% cargo run --release search 0 26889000
+Finished release [optimized] target(s) in 0.07s
+Running `target/release/number-persistence search 0 26889000`
+Found a new record: 11
+Found a new record: 25
+Found a new record: 39
+Found a new record: 77
+Found a new record: 679
+Found a new record: 6788
+Found a new record: 68889
+Found a new record: 2677889
+Found a new record: 26888999
+Overall record: 26888999
+```
+
+## License
+
+You're free to use this, but please credit me in any papers or discoveries. I'd
+also love to chat about this field, collaborate, and know what you're up to
+&mdash; please feel free to email me at jordan@whoami.sh.
 
 [OEIS Sequence A003001]: https://oeis.org/A003001
 [persistence of a number]: https://en.wikipedia.org/wiki/Persistence_of_a_number
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,80 @@
 extern crate clap;
 extern crate num;
 
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand};
 use num::bigint::*;
 use num::FromPrimitive;
+use num::One;
 use std::str::FromStr;
 
 // TODO(mcqueenjordan): Add optimized searches to find maximums within integer ranges.
 // TODO(mcqueenjordan): Add a command for iteratively checking ranges etc.
 
 fn main() {
-    let matches = App::new("number-persistence")
+    let cli = App::new("number-persistence")
         .version("0.1.0")
         .author("Jordan McQueen <jordan@whoami.sh>")
         .about("Multiplicative persistence checker.")
-        .arg(
-            Arg::with_name("number")
-                .required(true)
-                .takes_value(true)
-                .index(1)
-                .help("number to check"),
+        .subcommand(
+            SubCommand::with_name("check-multiplicative").arg(
+                Arg::with_name("number")
+                    .required(true)
+                    .takes_value(true)
+                    .index(1)
+                    .help("number to check"),
+            ),
+        )
+        .subcommand(
+            SubCommand::with_name("search")
+                .arg(
+                    Arg::with_name("begin")
+                        .required(true)
+                        .takes_value(true)
+                        .index(1)
+                        .help("Number at which to begin searching."),
+                )
+                .arg(
+                    Arg::with_name("end")
+                        .required(true)
+                        .takes_value(true)
+                        .index(2)
+                        .help("Upper bound. When to stop searching."),
+                ),
         )
         .get_matches();
-    let number: BigUint = FromStr::from_str(matches.value_of("number").unwrap()).unwrap();
-    println!("{}", calculate_multiplicative_persistence(number));
+
+    // TODO(mcqueenjordan): This is verbose and ugly, but I don't immediately see a better way.
+    match cli.subcommand_name().unwrap() {
+        "search" => search_for_maximum_multiplicative_persistence(
+            &FromStr::from_str(
+                cli.subcommand_matches("search")
+                    .unwrap()
+                    .value_of("begin")
+                    .unwrap(),
+            )
+            .unwrap(),
+            &FromStr::from_str(
+                cli.subcommand_matches("search")
+                    .unwrap()
+                    .value_of("end")
+                    .unwrap(),
+            )
+            .unwrap(),
+        ),
+        "check-multiplicative" => println!(
+            "{}",
+            calculate_multiplicative_persistence(
+                &FromStr::from_str(
+                    cli.subcommand_matches("check-multiplicative")
+                        .expect("Subcommand unwrap failed")
+                        .value_of("number")
+                        .unwrap()
+                )
+                .unwrap()
+            )
+        ),
+        _ => panic!("Unknown subcommand..."),
+    }
 }
 
 /// Calculates the multiplicative persistence of a number.
@@ -45,11 +96,12 @@ fn main() {
 /// let record_num: BigUint = FromStr::from_str("277777788888899")?
 /// assert!(calculate_multiplicative_persistence(record_num) == 11)
 /// ```
-fn calculate_multiplicative_persistence(number: BigUint) -> u32 {
+fn calculate_multiplicative_persistence(number: &BigUint) -> u32 {
     let mut persistence: u32 = 0;
-    let mut working_num: BigUint = number;
+    let mut working_num: BigUint = number.clone();
 
     // TODO(mcqueenjordan): better optimize this while condition
+    // TODO(mcqueenjordan): DP utilizing least-recently-used eviction
     while working_num >= FromStr::from_str("10").unwrap() {
         persistence += 1;
 
@@ -63,4 +115,33 @@ fn calculate_multiplicative_persistence(number: BigUint) -> u32 {
             });
     }
     return persistence;
+}
+
+// TODO(mcqueenjordan): Be smarter about skipping obviously bad digits.
+fn search_for_maximum_multiplicative_persistence(start: &BigUint, end: &BigUint) {
+    let mut working_num: BigUint = start.clone();
+    let mut record: BigUint = BigUint::one();
+    let mut max_seen: u32 = 0;
+
+    while working_num < *end {
+        // An optimization to skip past numbers with 0s in them.
+        let digits: String = working_num
+            .to_str_radix(10)
+            .chars()
+            .map(|c| match c {
+                '0' => '1',
+                _ => c,
+            })
+            .collect();
+        working_num = FromStr::from_str(&digits).unwrap();
+
+        let persistence = calculate_multiplicative_persistence(&working_num);
+        if persistence > max_seen {
+            max_seen = persistence;
+            record = working_num.to_owned();
+            println!("Found a new record: {}", record);
+        }
+        working_num = working_num + BigUint::one();
+    }
+    println!("Overall record: {}", record);
 }
